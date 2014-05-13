24d04ad0440727ab95118b1c601ce5008e4531c8
Shane Logsdon <shane@shanelogsdon.com>
2014-05-13 10:36:21 -0400

    fixing formatting issues

--- mix.exs
+++ mix.exs
@@ -14,15 +14,10 @@ defmodule Plugs.Mixfile do
 
   # Configuration for the OTP application
   def application do
-    [ applications: [:exlager],
+    [ applications: [],
       mod: { Sugar.App, [] } ]
   end
 
-  # Returns the list of dependencies in the format:
-  # { :foobar, git: "https://github.com/elixir-lang/foobar.git", tag: "0.1" }
-  #
-  # To specify particular versions, regardless of the tag, do:
-  # { :barbat, "~> 0.1", github: "elixir-lang/barbat" }
   defp deps(:prod) do
     [ { :cowboy, "~> 0.9", github: "extend/cowboy" },
       { :plug, "0.4.3" } ]
@@ -46,6 +41,6 @@ defmodule Plugs.Mixfile do
   defp package do
     %{contributors: ["Shane Logsdon"],
       licenses: ["MIT"],
-      links: %{"GitHub" => "https://github.com/sugar-framework/plugs"}
+      links: %{"GitHub" => "https://github.com/sugar-framework/plugs"}}
   end
 end
